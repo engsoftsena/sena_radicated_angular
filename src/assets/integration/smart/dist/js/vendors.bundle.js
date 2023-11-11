2d0687db8cc2746522b1d3b9a7837f037906d083
adisonzenemij <adisonzenemij@gmail.com>
2023-11-11 17:15:49 -0500

    Feat: Implementacion Modulos

--- src/assets/integration/smart/dist/js/vendors.bundle.js
+++ src/assets/integration/smart/dist/js/vendors.bundle.js
@@ -25418,11 +25418,22 @@ jQuery.fn.extend({
  * rgb2hex(hex_value)
  */
 function rgb2hex(rgb) {
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {
+  /*rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  function hex(x) {
+    return ("0" + parseInt(x).toString(16)).slice(-2);
+  }
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);*/
+  if (typeof rgb !== 'string') { return ''; }
+  if (typeof rgb === 'string') {
+    const matchRgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    if (!matchRgb) { return ''; }
+    if (matchRgb) {
+      function hex(x) {
         return ("0" + parseInt(x).toString(16)).slice(-2);
+      }
+      return "#" + hex(matchRgb[1]) + hex(matchRgb[2]) + hex(matchRgb[3]);
     }
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
 }
 /*!
  * jQuery throttle / debounce - v1.1 - 3/7/2010
