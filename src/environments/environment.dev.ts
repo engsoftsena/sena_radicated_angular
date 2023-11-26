52efc8c1ba60e40d32a27507ff297232540c281a
adisonzenemij <adisonzenemij@gmail.com>
2023-11-26 17:20:58 -0500

    Feat: Variables de Entorno

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const Environment = {
   production: true,
-  API_URL: 'https://dsx.adisonjimenez.work/staff_slim_seven/',
+  //API_URL: 'https://dsx.adisonjimenez.work/staff_slim_seven/',
+  API_URL: 'http://127.0.0.1:8857/engines/github/personnels/adisonzenemij/private/framework/staff_slim_seven/',
 };
