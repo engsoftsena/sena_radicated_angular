cf5792202bbfb9b67bff133837d893c1453fa077
adisonzenemij <adisonzenemij@gmail.com>
2023-12-10 01:55:57 -0500

    Feat: Implementacion Modulos

--- src/app/modules/internal/dashboard/dashboard.component.ts
+++ src/app/modules/internal/dashboard/dashboard.component.ts
@@ -61,145 +61,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     this.checkEndpoint();
   }
 
-  ngAfterViewInit() {
-    /*Highcharts.chart('container_basic_bar', {
-      chart: {
-        type: 'bar'
-      },
-      title: {
-        text: 'Historic World Population by Region',
-        align: 'left'
-      },
-      subtitle: {
-        text: 'Source: <a ' +
-          'href="https://en.wikipedia.org/wiki/List_of_continents_and_continental_subregions_by_population"' +
-          'target="_blank">Wikipedia.org</a>',
-        align: 'left'
-      },
-      xAxis: {
-        categories: ['Africa', 'America', 'Asia', 'Europe'],
-        title: {
-          text: null
-        },
-        gridLineWidth: 1,
-        lineWidth: 0
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Population (millions)',
-          align: 'high'
-        },
-        labels: {
-          overflow: 'justify'
-        },
-        gridLineWidth: 0
-      },
-      tooltip: {
-        valueSuffix: ' millions'
-      },
-      plotOptions: {
-        bar: {
-          borderRadius: '50%',
-          dataLabels: {
-            enabled: true
-          },
-          groupPadding: 0.1
-        }
-      },
-      legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 80,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor:
-          Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-        shadow: true
-      },
-      credits: {
-        enabled: false
-      },
-      series: [{
-        name: 'Year 1990',
-        data: [631, 727, 3202, 721]
-      }, {
-        name: 'Year 2000',
-        data: [814, 841, 3714, 726]
-      }, {
-        name: 'Year 2018',
-        data: [1276, 1007, 4561, 746]
-      }]
-    });
-
-    // Data retrieved from https://netmarketshare.com
-    Highcharts.chart('container_pie_chart', {
-      chart: {
-        plotBackgroundColor: null,
-        plotBorderWidth: null,
-        plotShadow: false,
-        type: 'pie'
-      },
-      title: {
-        text: 'Browser market shares in May, 2020',
-        align: 'left'
-      },
-      tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      accessibility: {
-        point: {
-          valueSuffix: '%'
-        }
-      },
-      plotOptions: {
-        pie: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-          }
-        }
-      },
-      series: [{
-        name: 'Brands',
-        colorByPoint: true,
-        data: [{
-          name: 'Chrome',
-          y: 70.67,
-          sliced: true,
-          selected: true
-        }, {
-          name: 'Edge',
-          y: 14.77
-        }, {
-          name: 'Firefox',
-          y: 4.86
-        }, {
-          name: 'Safari',
-          y: 2.63
-        }, {
-          name: 'Internet Explorer',
-          y: 1.53
-        }, {
-          name: 'Opera',
-          y: 1.40
-        }, {
-          name: 'Sogou Explorer',
-          y: 0.84
-        }, {
-          name: 'QQ',
-          y: 0.51
-        }, {
-          name: 'Other',
-          y: 2.6
-        }]
-      }]
-    });*/
-  }
+  ngAfterViewInit() {}
 
   modalClass() { expModalClass(); }
 
@@ -378,22 +240,6 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     if (report == 'pqrs') { this.resultPqrs(); }
   }
 
-  reportSettled() {
-
-  }
-
-  reportCausal() {
-
-  }
-
-  reportRequest() {
-
-  }
-
-  reportPqrs() {
-
-  }
-
   resultSettled() {
     const params = {
       groupBy: this.groupBy,
@@ -417,6 +263,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             buttonsData
           );
           this.isLoading = false;
+          this.reportGraphic('settled', 'Radicados', response.data);
         }
       },
       error: (err: any) => {
@@ -450,6 +297,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             buttonsData
           );
           this.isLoading = false;
+          this.reportGraphic('causal', 'Causales', response.data);
         }
       },
       error: (err: any) => {
@@ -483,6 +331,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             buttonsData
           );
           this.isLoading = false;
+          this.reportGraphic('request', 'Solicitudes', response.data);
         }
       },
       error: (err: any) => {
@@ -516,6 +365,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             buttonsData
           );
           this.isLoading = false;
+          //this.reportGraphic('pqrs', 'PQRS', response.data);
         }
       },
       error: (err: any) => {
@@ -523,6 +373,72 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         this.modalSystemJson(message, err);
       },
       complete: () => (false),
+    });
+  }
+
+  reportGraphic(report: string, title: string, responseData: any[]) {
+    let gpData = '';
+    if (report == 'settled') { gpData = 'gpBasicBarSettled'; }
+    if (report == 'causal') { gpData = 'gpBasicBarCausal'; }
+    if (report == 'request') { gpData = 'gpBasicBarRequest'; }
+    if (report == 'pqrs') { gpData = 'gpBasicBarPqrs'; }
+
+    responseData.sort((a, b) => {
+      const dateA = Date.parse(a.lbl_ap_settled_os_date);
+      const dateB = Date.parse(b.lbl_ap_settled_os_date);
+      return dateA - dateB;
+    });
+
+    const categories = responseData.map(item => item.lbl_ap_settled_os_date);
+    const seriesData = [{
+      colorByPoint: true,
+      data: responseData.map(
+        item => item.lbl_ap_settled_os_count
+      ),
+      name: 'Cantidades',
+    }];
+
+    Highcharts.chart(gpData, {
+      chart: {
+        type: 'bar',
+      },
+      title: {
+        text: `Reporte ${title}`,
+        align: 'center',
+      },
+      xAxis: {
+        categories: categories,
+        gridLineWidth: 1,
+        tickInterval: 1,
+      },
+      yAxis: {
+        allowDecimals: false,
+        gridLineWidth: 1,
+        min: 0,
+        labels: {
+          overflow: 'justify',
+        },
+        title: {
+          text: 'Valores',
+        },
+        tickInterval: 1,
+      },
+      legend: {
+        reversed: true,
+      },
+      plotOptions: {
+        series: {
+          borderRadius: '0%',
+          stacking: 'normal',
+          dataLabels: {
+            enabled: true,
+          },
+          groupPadding: 0.1,
+          pointPadding: 0.1,
+          pointWidth: 25,
+        },
+      },
+      series: seriesData,
     });
   }
 
