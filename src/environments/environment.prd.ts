4daed9a9b98e8739538f7f529d718cb93f3c1527
adisonzenemij <adisonzenemij@gmail.com>
2024-03-22 16:19:36 -0500

    Feat: Configuracion API

--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -1,4 +1,4 @@
 export const Environment = {
   production: true,
-  API_URL: 'https://psx.adisonjimenez.work/staff_slim_seven/',
+  API_URL: 'https://psx.adisonjimenez.work/sena_project_slim/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const Environment = {
   production: false,
-  API_URL: 'http://127.0.0.1:8857/engines/github/personnels/adisonzenemij/private/framework/staff_slim_seven/',
+  API_URL: 'http://127.0.0.1:8857/engines/github/adisonzenemij/sena_project_slim/',
 };
