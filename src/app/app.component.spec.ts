48ecf8f434a5c78c5752d02f873fa3bbdd798402
adisonzenemij <adisonzenemij@gmail.com>
2024-04-10 15:09:00 -0500

    Feat: Pruebas Unitarias

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project');
+    expect(app.AppCompTitle).toEqual('project');
   });
 
   it('should render title', () => {
