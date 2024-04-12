2c71f03226624413f13dbfd3b2f6be1ba61d58ff
adisonzenemij <adisonzenemij@gmail.com>
2024-04-11 20:24:45 -0500

    Feat: Pruebas Unitarias Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'APP PQRS'`, () => {
+  it(`should have as title 'sena_project_angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.AppCompTitle).toEqual('APP PQRS');
+    expect(app.title).toEqual('sena_project_angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('APP PQRS app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sena_project_angular app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'sena_project_angular';
   AppCompTitle = 'APP PQRS';
 }
