7408821d7e36c124ba72236632ea640501aa8308
adisonzenemij <adisonzenemij@gmail.com>
2024-05-04 19:54:40 -0500

    Feat: Configuracion Servidor

--- src/app/services/functions/endpoint/endpoint.service.ts
+++ src/app/services/functions/endpoint/endpoint.service.ts
@@ -75,11 +75,11 @@ export class EndpointService {
 
   getCurrentExt(): string {
     const { hostname } = window.location;
-    console.log('hostname', hostname);
     const parts = hostname.split('.');
-    console.log('parts', parts);
     if (parts.length > 1) {
-      return parts[parts.length - 1];
+      return parts[
+        parts.length - 1
+      ];
     }
     return '';
   }
