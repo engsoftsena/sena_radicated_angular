e30b8be44dc818f83103dd71ea4d70f5f6a5520e
adisonzenemij <adisonzenemij@gmail.com>
2024-08-30 09:42:50 -0500

    Feat: Configuracion Servidor

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,8 +1,8 @@
 export const Environment = {
   production: true,
-  backend_app: 'https://dsx.engsoft.app/sena_pqrs_slim/',
-  backend_net: 'https://dsx.adisonjimenez.net/sena_pqrs_slim/',
-  backend_test: 'http://127.0.0.1:8857/engines/github/adisonzenemij/sena_pqrs_slim/',
-  backend_work: 'https://dsx.adisonjimenez.work/sena_pqrs_slim/',
+  backend_app: 'https://dsx.engsoft.app/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_net: 'https://dsx.adisonjimenez.net/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_test: 'http://127.0.0.1:8857/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_work: 'https://dsx.adisonjimenez.work/github/engsoftapp/sena/sena_radicated_slim/',
   domain_tmpl: 'https://tmplwrapsmart.engsoft.app/version.4.5.1/html-full/dist',
 };
--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -1,8 +1,8 @@
 export const Environment = {
   production: true,
-  backend_app: 'https://psx.engsoft.app/sena_pqrs_slim/',
-  backend_net: 'https://psx.adisonjimenez.net/sena_pqrs_slim/',
-  backend_test: 'http://127.0.0.1:7857/engines/github/adisonzenemij/sena_pqrs_slim/',
-  backend_work: 'https://psx.adisonjimenez.work/sena_pqrs_slim/',
+  backend_app: 'https://psx.engsoft.app/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_net: 'https://psx.adisonjimenez.net/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_test: 'http://127.0.0.1:7857/github/engsoftapp/sena/sena_radicated_slim/',
+  backend_work: 'https://psx.adisonjimenez.work/github/engsoftapp/sena/sena_radicated_slim/',
   domain_tmpl: 'https://tmplwrapsmart.engsoft.app/version.4.5.1/html-full/dist',
 };
